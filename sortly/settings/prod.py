90b8ed2364b347a8890f356d0a5c0b4b93448034
Sathwik <sathwikbhimavarapu@gmail.com>
2022-01-01 16:35:00 +0530

    Change 3

--- sortly/settings/prod.py
+++ sortly/settings/prod.py
@@ -15,3 +15,18 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+CACHE_TTL = 60
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
